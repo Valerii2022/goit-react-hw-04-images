9a6d09e289399bd66c78c2a67087e0e116838e7b
Valerii Pometun <vmpometun@gmail.com>
2023-05-18 11:50:45 +0300

    refactoring App & Modal almoust done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { PixabayAPI } from 'services/pixabay-api';
 import Notiflix from 'notiflix';
 import { Container } from './styled';
@@ -9,93 +9,163 @@ import Loader from 'components/Loader';
 
 const pixabayApi = new PixabayAPI();
 
-// const New =()=>{}
-console.log('test');
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [pageNumber, setPageNumber] = useState(1);
+  const [cards, setCards] = useState([]);
+  const [loaderVisible, setLoaderVisible] = useState(false);
+  const [loadMoreBtn, setLoadMoreBtn] = useState(false);
 
-class App extends Component {
-  state = {
-    query: '',
-    pageNumber: 1,
-    cards: [],
-    loaderVisible: false,
-    loadMoreBtn: false,
-  };
-
-  async componentDidUpdate(_, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.pageNumber !== this.state.pageNumber
-    ) {
-      this.setState({ loaderVisible: true });
+  useEffect(() => {
+    if (query !== '') {
+      setLoaderVisible(true);
       try {
-        const { data } = await pixabayApi.fetchPhotos(
-          this.state.query,
-          this.state.pageNumber
-        );
-        this.handleSuccessFetch(data);
+        const fetchData = async () => {
+          const { data } = await pixabayApi.fetchPhotos(query, pageNumber);
+          console.log(data);
+          handleSuccessFetch(data);
+          return data;
+        };
+        console.log(fetchData());
       } catch (error) {
-        this.handleErrorFetch(error);
+        handleErrorFetch(error);
       } finally {
-        this.setState({ loaderVisible: false });
+        setLoaderVisible(false);
       }
     }
-  }
+  }, [pageNumber, query]);
 
-  handleSubmitForm = query => {
-    if (this.state.query !== query) {
-      this.setState({
-        cards: [],
-        pageNumber: 1,
-        query,
-        loadMoreBtn: false,
-      });
+  const handleSubmitForm = currentQuery => {
+    if (query !== currentQuery) {
+      setQuery(currentQuery);
+      setPageNumber(1);
+      setLoadMoreBtn(false);
+      setCards([]);
     }
   };
 
-  handleLoadMoreBtnClick = () => {
-    this.setState(prevState => ({
-      pageNumber: prevState.pageNumber + 1,
-      loadMoreBtn: true,
-    }));
+  const handleLoadMoreBtnClick = () => {
+    setLoadMoreBtn(true);
+    setPageNumber(pageNumber => pageNumber + 1);
   };
 
-  handleSuccessFetch(data) {
+  const handleSuccessFetch = data => {
     if (data.totalHits === 0) {
-      this.setState({ loadMoreBtn: false });
+      setLoadMoreBtn(false);
       Notiflix.Notify.failure(
         'Sorry, there are no images matching your search query. Please try again.'
       );
       return;
     }
-    this.setState(prevState => ({
-      loaderVisible: false,
-      cards: [...prevState.cards, ...data.hits],
-      loadMoreBtn: true,
-    }));
+    setLoaderVisible(false);
+    setLoadMoreBtn(true);
+    setCards(cards => [...cards, ...data.hits]);
     if (data.hits.length < 12) {
-      this.setState({ loadMoreBtn: false });
+      setLoadMoreBtn(false);
       Notiflix.Notify.info(
         `We're sorry, but you've reached the end of search results.`
       );
     }
-  }
+  };
 
-  handleErrorFetch(error) {
+  const handleErrorFetch = error => {
     Notiflix.Notify.failure(error.message);
-  }
+  };
 
-  render() {
-    return (
-      <Container>
-        <Searchbar handleSubmit={this.handleSubmitForm} />
-        <ImageGallery cards={this.state.cards} />
-        {this.state.loaderVisible && <Loader />}
-        {this.state.loadMoreBtn && (
-          <Button handleSubmit={this.handleLoadMoreBtnClick} />
-        )}
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Searchbar handleSubmit={handleSubmitForm} />
+      <ImageGallery cards={cards} />
+      {loaderVisible && <Loader />}
+      {loadMoreBtn && <Button handleSubmit={handleLoadMoreBtnClick} />}
+    </Container>
+  );
+};
+
+// class App extends Component {
+//   state = {
+//     query: '',
+//     pageNumber: 1,
+//     cards: [],
+//     loaderVisible: false,
+//     loadMoreBtn: false,
+//   };
+
+//   async componentDidUpdate(_, prevState) {
+//     if (
+//       prevState.query !== this.state.query ||
+//       prevState.pageNumber !== this.state.pageNumber
+//     ) {
+//       this.setState({ loaderVisible: true });
+//       try {
+//         const { data } = await pixabayApi.fetchPhotos(
+//           this.state.query,
+//           this.state.pageNumber
+//         );
+//         this.handleSuccessFetch(data);
+//       } catch (error) {
+//         this.handleErrorFetch(error);
+//       } finally {
+//         this.setState({ loaderVisible: false });
+//       }
+//     }
+//   }
+
+//   handleSubmitForm = query => {
+//     if (this.state.query !== query) {
+//       this.setState({
+//         cards: [],
+//         pageNumber: 1,
+//         query,
+//         loadMoreBtn: false,
+//       });
+//     }
+//   };
+
+//   handleLoadMoreBtnClick = () => {
+//     this.setState(prevState => ({
+//       pageNumber: prevState.pageNumber + 1,
+//       loadMoreBtn: true,
+//     }));
+//   };
+
+//   handleSuccessFetch(data) {
+//     if (data.totalHits === 0) {
+//       this.setState({ loadMoreBtn: false });
+//       Notiflix.Notify.failure(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
+//       return;
+//     }
+//     this.setState(prevState => ({
+//       loaderVisible: false,
+//       cards: [...prevState.cards, ...data.hits],
+//       loadMoreBtn: true,
+//     }));
+//     if (data.hits.length < 12) {
+//       this.setState({ loadMoreBtn: false });
+//       Notiflix.Notify.info(
+//         `We're sorry, but you've reached the end of search results.`
+//       );
+//     }
+//   }
+
+//   handleErrorFetch(error) {
+//     Notiflix.Notify.failure(error.message);
+//   }
+
+//   render() {
+//     return (
+//       <Container>
+//         <Searchbar handleSubmit={this.handleSubmitForm} />
+//         <ImageGallery cards={this.state.cards} />
+//         {this.state.loaderVisible && <Loader />}
+//         {this.state.loadMoreBtn && (
+//           <Button handleSubmit={this.handleLoadMoreBtnClick} />
+//         )}
+//       </Container>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,75 +1,74 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import { useEffect } from 'react';
 import { Overlay, ImgModal } from './styled';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.querySelector('#modal-root');
 
-// const Modal = ({ url, openModal }) => {
-//   const handleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       openModal();
-//     }
-//   };
+const Modal = ({ url, openModal }) => {
+  // const handleKeyDown = e => {
+  //   if (e.code === 'Escape') {
+  //     openModal();
+  //   }
+  // };
 
-//   useEffect(() => {
-//     console.log('1');
-//     window.addEventListener('keydown', handleKeyDown);
-//   });
+  // useEffect(() => {
+  //   window.addEventListener('keydown', handleKeyDown);
+  // });
 
-//   useEffect(() => {
-//     console.log('2');
-//     window.remove.EventListener('keydown', handleKeyDown);
-//   }, []);
+  // useEffect(() => {
+  //   console.log('2');
+  //   window.remove.EventListener('keydown', handleKeyDown);
+  // }, [handleKeyDown]);
 
-//   const handleBackdropClick = event => {
-//     if (event.currentTarget === event.target) {
-//       openModal();
-//     }
-//   };
-
-//   return createPortal(
-//     <Overlay onClick={handleBackdropClick}>
-//       <ImgModal>
-//         <img src={url} alt="" />
-//       </ImgModal>
-//     </Overlay>,
-//     modalRoot
-//   );
-// };
-
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
-
-  handleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.openModal();
-    }
-  };
-
-  handleBackdropClick = event => {
+  const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.openModal();
+      openModal();
     }
   };
 
-  render() {
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ImgModal>
-          <img src={this.props.url} alt="" />
-        </ImgModal>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ImgModal>
+        <img src={url} alt="" />
+      </ImgModal>
+    </Overlay>,
+    modalRoot
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.openModal();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.openModal();
+//     }
+//   };
+
+//   render() {
+//     return createPortal(
+//       <Overlay onClick={this.handleBackdropClick}>
+//         <ImgModal>
+//           <img src={this.props.url} alt="" />
+//         </ImgModal>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
 
 export default Modal;
 
