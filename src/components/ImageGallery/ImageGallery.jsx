fd0085e7772ddc94b7e5b2225f025cc6301ea3a6
Valerii Pometun <vmpometun@gmail.com>
2023-05-18 08:57:36 +0300

    Modal & App start

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -40,52 +40,6 @@ const ImageGallery = ({ cards }) => {
   );
 };
 
-// class ImageGallery extends Component {
-//   state = {
-//     url: '',
-//     openModal: false,
-//   };
-
-//   addModalUrl = e => {
-//     const modalUrl = this.props.cards.filter(card => {
-//       return Number(card.id) === Number(e.currentTarget.id);
-//     });
-//     this.setState({
-//       url: modalUrl[0].largeImageURL,
-//       openModal: true,
-//     });
-//   };
-
-//   handleOpenModal = () => {
-//     this.setState({
-//       openModal: false,
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <Gallery>
-//           {this.props.cards.map(({ id, webformatURL, tags }) => {
-//             return (
-//               <ImageGalleryItem
-//                 id={id}
-//                 key={id}
-//                 onClick={this.addModalUrl}
-//                 src={webformatURL}
-//                 alt={tags}
-//               />
-//             );
-//           })}
-//         </Gallery>
-//         {this.state.openModal && (
-//           <Modal url={this.state.url} openModal={this.handleOpenModal} />
-//         )}
-//       </>
-//     );
-//   }
-// }
-
 export default ImageGallery;
 
 ImageGallery.propTypes = {
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -35,43 +35,6 @@ const Searchbar = ({ handleSubmit }) => {
   );
 };
 
-// class Searchbar extends Component {
-//   state = {
-//     query: '',
-//     pageNumber: 1,
-//   };
-
-//   addQueryName = event => {
-//     this.setState({ query: event.target.value });
-//   };
-
-//   handleSubmitForm = e => {
-//     e.preventDefault();
-//     this.props.handleSubmit(this.state.query);
-//   };
-
-//   render() {
-//     return (
-//       <Header>
-//         <Form onSubmit={this.handleSubmitForm}>
-//           <SearchFormBtn type="submit" disabled={!this.state.query.trim()}>
-//             <SearchIcon width="30" height="30" />
-//           </SearchFormBtn>
-
-//           <SearchFormInput
-//             value={this.state.query}
-//             onChange={this.addQueryName}
-//             type="text"
-//             autoComplete="off"
-//             autoFocus
-//             placeholder="Search images and photos"
-//           />
-//         </Form>
-//       </Header>
-//     );
-//   }
-// }
-
 export default Searchbar;
 
 Searchbar.propTypes = {
